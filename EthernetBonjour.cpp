0162af5839109d64183718eae2e79c4a1bfe3a26
Jonas Ohland <jonas.ohland@gmail.com>
2019-11-26 16:50:55 +0100

    use Ethernet3 Library

--- EthernetBonjour.cpp
+++ EthernetBonjour.cpp
@@ -24,8 +24,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <Arduino.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
+#include <Ethernet3.h>
+#include <EthernetUdp3.h>
 
 extern "C" {
    #include <utility/EthernetUtil.h>
